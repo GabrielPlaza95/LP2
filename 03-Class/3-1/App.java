53e81add5c91f2615fa85e5313b7a4b63377eb25
Gabriel Plaza <12886990+GabrielPlaza95@users.noreply.github.com>
2021-09-07 18:40:59 -0300

    Exercício 5.1

--- 03-Class/3-1/App.java
+++ 03-Class/3-1/App.java
@@ -4,6 +4,8 @@ import java.awt.Graphics;
 import java.awt.Graphics2D;
 import java.awt.event.*;
 import javax.swing.*;
+import java.util.ArrayList;
+import java.util.Random;
 
 public class App {
     public static void main (String[] args) {
@@ -12,7 +14,20 @@ public class App {
 }
 
 class Hello2DFrame extends JFrame {
+	ArrayList<Rectangle> rectangleList;
+	ArrayList<Ellipse> ellipseList;
+
     public Hello2DFrame () {
+		Random rand = new Random();
+
+        this.setTitle("Java2D");
+        this.setSize(1000, 1000);
+        this.setVisible(true);
+		this.getContentPane().setBackground(Color.white);
+
+		this.rectangleList = new ArrayList<Rectangle>();
+		this.ellipseList = new ArrayList<Ellipse>();
+
         this.addWindowListener (
             new WindowAdapter() {
                 public void windowClosing (WindowEvent e) {
@@ -21,22 +36,41 @@ class Hello2DFrame extends JFrame {
             }
         );
 
-        this.setTitle("Java2D");
-        this.setSize(1000, 1000);
-        this.setVisible(true);
-		this.getContentPane().setBackground(Color.white);
+		this.addKeyListener (
+			new KeyAdapter() {
+				public void keyPressed (KeyEvent evt) {
+					if (evt.getKeyChar() == 'e') {
+						int x = rand.nextInt(800);
+						int y = rand.nextInt(800);
+						int w = rand.nextInt(200);
+						int h = rand.nextInt(200);
+
+						ellipseList.add(new Ellipse(x, y, w, h, Color.pink, Color.pink));
+						repaint();
+					} 
+
+					if (evt.getKeyChar() == 'r') {
+						int x = rand.nextInt(800);
+						int y = rand.nextInt(800);
+						int w = rand.nextInt(200);
+						int h = rand.nextInt(200);
+
+						rectangleList.add(new Rectangle(x, y, w, h, Color.yellow, Color.yellow));
+						repaint();
+					} 
+				}
+			}
+		);
     }
 
     public void paint (Graphics g) {
         super.paint(g);
         Graphics2D g2d = (Graphics2D) g;
 
-		Rectangle rectangle = new Rectangle(200, 100, 100, 200, Color.red, Color.green);
-		Ellipse ellipse = new Ellipse(700, 100, 150, 150, Color.green, Color.blue);
-		Triangle triangle = new Triangle(500, 500, 300, 100, Color.blue, Color.red);
+		for (Rectangle rectangle : this.rectangleList)
+			rectangle.paint(g2d);
 
-		rectangle.paint(g2d);
-		ellipse.paint(g2d);
-		triangle.paint(g2d);
+		for (Ellipse ellipse : this.ellipseList)
+			ellipse.paint(g2d);
     }
 }
